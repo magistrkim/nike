ca8a317859ea6a8d618f9f133f2af074963f42f9
Nataliya Kim <magistr1982@gmail.com>
2023-10-02 19:51:29 +0300

    Add components and structure

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Nav from './Nav';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+
+export {
+  Nav,
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+};
