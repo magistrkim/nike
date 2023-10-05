9e6aac96588903c85fffe84c1ffcfe77f11808f4
Nataliya Kim <magistr1982@gmail.com>
2023-10-05 08:45:38 +0300

    Add SuperQuality section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,11 +6,13 @@ const Button = ({ label, iconURL }) => {
     bg-coral-red rounded-full text-white border-coral-red"
     >
       {label}
-      <img
-        src={iconURL}
-        alt="arrow right icon"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
